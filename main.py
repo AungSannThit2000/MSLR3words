26f5d5ee29c744912278103778efe2d11c64cfc8
Aung Sann Thit <90388214+AungSannThit2000@users.noreply.github.com>
2023-03-28 23:23:42 +0630

    changed turn server link

--- main.py
+++ main.py
@@ -182,8 +182,9 @@ elif choice_s == "Webcam Hand Detection":
     st.markdown(webcam_subheader, unsafe_allow_html=True)
     webcam_secondheader = '<p style="font-family:Monospace; color:LightSteelBlue; font-size: 10px;">Exception : It will take one or two seconds before it starts detection.</p>'
     st.markdown(webcam_secondheader, unsafe_allow_html=True)
+
     RTC_CONFIGURATION = RTCConfiguration(
-        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+        {"iceServers": [{"urls": ["relay1.expressturn.com:3478"]}]}
     )
     webrtc_ctx = webrtc_streamer(
         key="WYH",
